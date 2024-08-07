c1c920302e1782930ccdb4c124bc9b6deb8a29b1
Asher Serussi <ashse@trondelagfylke.no>
2024-08-07 13:21:01 +0200

    SIF API version 6.0

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const AccessGroupService = require('./lib/accessgroup-service')
-const CaseService = require('./lib/case-service')
-const ContactService = require('./lib/contact-service')
-const DocumentService = require('./lib/document-service')
-const EstateService = require('./lib/estate-service')
-const FileService = require('./lib/file-service')
-const MyCasesService = require('./lib/mycases-service')
-const ProjectService = require('./lib/project-service')
-const UserService = require('./lib/user-service')
-const SupportService = require('./lib/support-service')
+const AccessGroupService = require("./lib/accessgroup-service");
+const ActivityService = require("./lib/activity-service");
+const CaseService = require("./lib/case-service");
+const ContactService = require("./lib/contact-service");
+const DocumentService = require("./lib/document-service");
+const EstateService = require("./lib/estate-service");
+const FileService = require("./lib/file-service");
+const MyCasesService = require("./lib/mycases-service");
+const ProjectService = require("./lib/project-service");
+const RevisionService = require("./lib/revision-service");
+const SupportService = require("./lib/support-service");
+const UserService = require("./lib/user-service");
 
-module.exports = options => {
-  if (!options) throw TypeError('Missing required input: options')
-  if (!options.host) throw TypeError('Missing required input: options.host')
-  if (!options.authkey) throw TypeError('Missing required input: options.authkey')
+module.exports = (options) => {
+  if (!options) throw TypeError("Missing required input: options");
+  if (!options.host) throw TypeError("Missing required input: options.host");
+  if (!options.authkey)
+    throw TypeError("Missing required input: options.authkey");
 
   return {
     AccessGroupService: AccessGroupService(options),
+    ActivityService: ActivityService(options),
     CaseService: CaseService(options),
     ContactService: ContactService(options),
     DocumentService: DocumentService(options),
@@ -23,7 +27,8 @@ module.exports = options => {
     FileService: FileService(options),
     MyCasesService: MyCasesService(options),
     ProjectService: ProjectService(options),
+    RevisionService: RevisionService(options),
     SupportService: SupportService(options),
-    UserService: UserService(options)
-  }
-}
+    UserService: UserService(options),
+  };
+};
